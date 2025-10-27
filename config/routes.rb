7e27b3b1b462fa4240d578d8caa62ab8fe40fedf
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-10-27 15:50:22 +0900

    homeコントローラを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "home#new"
+  resources :home, only: [ :index, :new ]
   resources :records
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
