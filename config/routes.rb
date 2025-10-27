167a5f5f30fa61165c7fbe917c0f62274d1d057b
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-10-27 18:34:39 +0900

    deviseを組み込んだUserモデルを生成した

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :home, only: [ :index, :new ]
   get "about/index"
   resources :records
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
