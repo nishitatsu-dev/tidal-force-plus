89ece860e742e03ab4a97735f398649de56618cd
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-11-02 09:44:57 +0900

    Userモデルにomniauth用の設定を追記した

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@ class User < ApplicationRecord
   has_many :sns_credentials, dependent: :destroy
   has_many :records, dependent: :destroy
   has_many :record_titles, dependent: :destroy
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data["email"]).first
+
+    unless user
+      user = User.create(email: data["email"], password: Devise.friendly_token[0, 20])
+      user.skip_confirmation!
+    end
+    user
+  end
 end
