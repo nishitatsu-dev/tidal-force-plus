f69097a0e81003c017f5a7c734f2194200d8df1e
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-11-02 22:55:16 +0900

    set_flash_messageのエラー対応。OmniAuth Google OAuth2 Strategy gemのREADMEに従い修正した

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,8 +5,8 @@ class Users::OmniauthCallbacksController < ApplicationController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
     else
       # Useful for debugging login failures. Uncomment for development.
       session["devise.google_data"] = request.env["omniauth.auth"].except("extra") # Removing extra as it can overflow some session stores
