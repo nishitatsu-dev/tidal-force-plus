a4394a696664ac506b596ebd03f1d8474d8ccaa3
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-11-02 09:43:25 +0900

    omniauth_callbacksコントローラを生成し、google用の設定を追記した

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,20 @@
+class Users::OmniauthCallbacksController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :google_oauth2
+
+  def google_oauth2
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+    else
+      # Useful for debugging login failures. Uncomment for development.
+      session["devise.google_data"] = request.env["omniauth.auth"].except("extra") # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
