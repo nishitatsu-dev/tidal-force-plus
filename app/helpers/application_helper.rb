f489e599ead97cf76eab5b9bcdbc1373edd94eb3
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-11-04 18:14:25 +0900

    「文字列を改行して表示する」メソッドを移植した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def display_line_breaks(original_text)
+    original_text.nil? ? nil : safe_join(original_text.split("\n"), tag.br)
+  end
+
   def each_home_index_path
     user_signed_in? ? memo_index_home_path(current_user.id) : home_index_path
   end
