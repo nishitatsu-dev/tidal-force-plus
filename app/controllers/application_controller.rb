884e19412171644e2c9028566d11956a7be234a2
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-11-04 18:04:17 +0900

    「ある日の測定結果」を作るメソッド群を移植した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,27 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  private
+  def get_db_records(date)
+    date_hour_0000 = Time.zone.parse(date + " 00:00")
+    date_hour_2300 = Time.zone.parse(date + " 23:00")
+    current_user.records.where(recorded_at: (date_hour_0000..date_hour_2300))
+  end
+
+  def make_hourly_records(date, db_records)
+    time_indexed_records = db_records.index_by { |record| record.recorded_at.hour }
+    date_hours = (0..23).map { |hour| Time.zone.parse(date + " " + format("%02d:00", hour)) }
+    (0..23).map do |hour|
+      time_indexed_records[hour] || Record.new(recorded_at: date_hours[hour])
+    end
+  end
+
+  def make_one_day_records
+    Time.use_zone(session[:timezone]) do
+      date = Date.parse(session[:first_date]).advance(days: session[:page_id]).strftime("%Y-%m-%d")
+      db_records = get_db_records(date)
+      make_hourly_records(date, db_records)
+    end
+  end
 end
