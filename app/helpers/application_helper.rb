0da0a83d6194b33fdd6148326ce8d5bca4306698
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-11-04 18:14:48 +0900

    「RecordのTurboFrameによる表示」で使うメソッドを移植した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def extract_date_hour(record)
+    record.recorded_at.in_time_zone(session[:timezone]).strftime("%m-%d_%H")
+  end
+
   def display_line_breaks(original_text)
     original_text.nil? ? nil : safe_join(original_text.split("\n"), tag.br)
   end
