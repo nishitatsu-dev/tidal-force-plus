a1c107b6fcdf1d46463d3b7053568eb0f918df5a
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-12-13 12:26:32 +0900

    sns認証のボタンを、deviseのビュー以外でも使えるようにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,4 +33,16 @@ module ApplicationHelper
       }
     }
   end
+
+  def devise_mapping
+    Devise.mappings[:user]
+  end
+
+  def resource_class
+    devise_mapping.to
+  end
+
+  def resource_name
+    devise_mapping.name
+  end
 end
