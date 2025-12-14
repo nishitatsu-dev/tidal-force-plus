a24cefbed246415637d15609044848b95bae6d85
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-12-15 07:43:17 +0900

    「戻る」リンクが意図した画面に戻るように修正した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,10 +7,6 @@ module ApplicationHelper
     original_text.nil? ? nil : safe_join(original_text.split("\n"), tag.br)
   end
 
-  def each_home_index_path
-    user_signed_in? ? memo_index_home_path(current_user.id) : home_index_path
-  end
-
   def default_meta_tags
     {
       site: "Tidal Force plus",
