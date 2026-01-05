c9e5f431883470cfdae1940e138677d98ca4aff6
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-01-05 18:10:24 +0900

    メモのタイトル欄のテストをシステムテストとして追加した（Hotwireを使っているので）

---
+++ test/system/record_titles_test.rb
@@ -0,0 +1,44 @@
+require "application_system_test_case"
+
+class RecordsTest < ApplicationSystemTestCase
+  test "タイトルの初期値からの変更" do
+    sign_in_as(users(:alice))
+    click_button "グラフ・表・メモを更新"
+
+    first('[aria-label="タイトルを初期値から変更する"]').click
+    page.has_field?("record_title[title]")
+    fill_in "record_title_title", with: "ケナフ2"
+    click_on "更新"
+    page.has_no_field?("record_title[title]")
+
+    assert_text "ケナフ2"
+  end
+
+  test "タイトルの編集" do
+    sign_in_as(users(:alice))
+    click_button "グラフ・表・メモを更新"
+    assert_text "レタス"
+
+    first('[aria-label="タイトルを編集する"]').click
+    page.has_field?("record_title[title]")
+    fill_in "record_title_title", with: "ケナフ1"
+    click_on "更新"
+    page.has_no_field?("record_title[title]")
+
+    assert_no_text "レタス"
+    assert_text "ケナフ1"
+  end
+
+  test "タイトルのリセット" do
+    sign_in_as(users(:alice))
+    click_button "グラフ・表・メモを更新"
+    assert_text "レタス"
+
+    first('[aria-label="タイトルをリセットする"]').click
+    page.has_text?("本当に削除しますか？")
+    click_on "OK"
+    page.has_no_text?("本当に削除しますか？")
+
+    assert_no_text "レタス"
+  end
+end
