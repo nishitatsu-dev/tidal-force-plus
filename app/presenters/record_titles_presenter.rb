e96eee4f871bdaae2c673ad3ff1767aa4d3201ba
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-02-01 23:18:26 +0900

    マジックナンバーになっていたので修正した

--- app/presenters/record_titles_presenter.rb
+++ app/presenters/record_titles_presenter.rb
@@ -1,11 +1,14 @@
 class RecordTitlesPresenter
+  MIN_COLUMN_NUMBER = 0
+  MAX_COLUMN_NUMBER = 5
+
   def initialize(user)
     @user = user
   end
 
   def complete_record_titles
     indexed_titles = @user.record_titles.index_by { |title| title.column_number }
-    (0..5).map do |column_number|
+    (MIN_COLUMN_NUMBER..MAX_COLUMN_NUMBER).map do |column_number|
       indexed_titles[column_number] || RecordTitle.new(column_number:, title: "No.#{column_number + 1}")
     end
   end
--- app/presenters/records_presenter.rb
+++ app/presenters/records_presenter.rb
@@ -1,12 +1,15 @@
 class RecordsPresenter
+  MIN_HOUR = 0
+  MAX_HOUR = 23
+
   def initialize(user)
     @user = user
   end
 
   def complete_one_day_records(date)
     indexed_records = @user.records.records_on(date).index_by { |record| record.recorded_at.hour }
-    date_hours = (0..23).map { |hour| Time.zone.parse(date + " " + format("%02d:00", hour)) }
-    (0..23).map do |hour|
+    date_hours = (MIN_HOUR..MAX_HOUR).map { |hour| Time.zone.parse(date + " " + format("%02d:00", hour)) }
+    (MIN_HOUR..MAX_HOUR).map do |hour|
       indexed_records[hour] || Record.new(recorded_at: date_hours[hour])
     end
   end
