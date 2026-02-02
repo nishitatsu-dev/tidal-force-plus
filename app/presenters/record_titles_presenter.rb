c03281bcd82b3ab95e349172e80892795c9bed6f
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-02-02 18:39:47 +0900

    RecordTitlesPresenterのテストを作った。併せて、「フィクスチャの追加」と「RecordTitlesPresenterの記述変更」を行なった

--- app/presenters/record_titles_presenter.rb
+++ app/presenters/record_titles_presenter.rb
@@ -1,6 +1,5 @@
 class RecordTitlesPresenter
-  MIN_COLUMN_NUMBER = 0
-  MAX_COLUMN_NUMBER = 5
+  COLUMN_NUMBER_RANGE = 0..5
 
   def initialize(user)
     @user = user
@@ -8,7 +7,7 @@ class RecordTitlesPresenter
 
   def complete_record_titles
     indexed_titles = @user.record_titles.index_by { |title| title.column_number }
-    (MIN_COLUMN_NUMBER..MAX_COLUMN_NUMBER).map do |column_number|
+    COLUMN_NUMBER_RANGE.map do |column_number|
       indexed_titles[column_number] || RecordTitle.new(column_number:, title: "No.#{column_number + 1}")
     end
   end
---
+++ test/presenters/record_titles_presenter_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class RecordTitlesPresenterTest < ActionDispatch::IntegrationTest
+  test "タイトルの表示欄に合うように、「保存されたタイトル」と「デフォルトのタイトル」を統合して返す" do
+    user = users(:carol)
+    presenter = RecordTitlesPresenter.new(user)
+    complete_titles = presenter.complete_record_titles
+
+    assert_equal RecordTitlesPresenter::COLUMN_NUMBER_RANGE.size, complete_titles.size
+
+    complete_titles.each_with_index do |title, index|
+      if user.record_titles.exists?(column_number: index)
+        assert_equal user.record_titles.find_by(column_number: index).title, title.title
+      else
+        assert_equal "No.#{index + 1}", title.title
+      end
+    end
+  end
+end
