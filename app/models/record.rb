0dcc7d12b27434dad433b769856d8f8b0da456dc
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-02-03 17:33:08 +0900

    Recordモデルにバリデーションを導入した。併せて、モデルで定義した定数を使うように、関連部分を変更した

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,18 @@
 class Record < ApplicationRecord
+  MEMO_MAX_LENGTH = 150
+  COLUMN_MAX_VALUE = 9999.999
+  COLUMN_MIN_VALUE = -999.999
+
   belongs_to :user
+
+  validates :recorded_at, presence: true
+  validates :column_0, numericality: { in: COLUMN_MIN_VALUE..COLUMN_MAX_VALUE }, allow_nil: true
+  validates :column_1, numericality: { in: COLUMN_MIN_VALUE..COLUMN_MAX_VALUE }, allow_nil: true
+  validates :column_2, numericality: { in: COLUMN_MIN_VALUE..COLUMN_MAX_VALUE }, allow_nil: true
+  validates :column_3, numericality: { in: COLUMN_MIN_VALUE..COLUMN_MAX_VALUE }, allow_nil: true
+  validates :column_4, numericality: { in: COLUMN_MIN_VALUE..COLUMN_MAX_VALUE }, allow_nil: true
+  validates :column_5, numericality: { in: COLUMN_MIN_VALUE..COLUMN_MAX_VALUE }, allow_nil: true
+  validates :memo, length: { maximum: MEMO_MAX_LENGTH }
 
   scope :records_on, ->(date) { where(recorded_at: Time.zone.parse(date).all_day) }
 end
