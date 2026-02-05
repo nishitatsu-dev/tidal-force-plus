c1ae0796e510d556fe3c957a9108d6403f8a6b25
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-02-05 18:43:25 +0900

    deviseのTrackableを使っていないので、関連項目を削除した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_05_091517) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_05_093347) do
   create_table "record_titles", force: :cascade do |t|
     t.integer "column_number", null: false
     t.string "title"
@@ -50,11 +50,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_05_091517) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
