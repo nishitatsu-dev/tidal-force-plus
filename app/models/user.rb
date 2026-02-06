52fd896fb27fa6c4a2fe5ef6f8de015f101af7bf
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-02-06 17:36:22 +0900

    deviseの「デフォルトで入る設定だが使っていないrecoverable」を機能停止した

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :timeoutable,
+         :rememberable, :validatable, :timeoutable,
          :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
   has_many :sns_credentials, dependent: :destroy
