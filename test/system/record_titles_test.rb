78dd3bf404e7ff4a90dcf1bae3ba573580381b5a
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-02-07 18:12:50 +0900

    フレーキーなテスト対策として、要素の探索範囲を絞り込んだ

--- test/system/record_titles_test.rb
+++ test/system/record_titles_test.rb
@@ -4,42 +4,53 @@ class RecordsTest < ApplicationSystemTestCase
   test "タイトルの初期値からの変更" do
     sign_in_as(users(:alice))
     click_button "グラフ・表・メモの表示更新"
-    assert_text "alice's title 1"
+    within 'div[aria-label="各数値データのタイトル"] turbo-frame#record_title_1' do
+      assert_text "No.2"
+    end
 
-    find('[aria-label="各数値データのタイトル"]').first('[aria-label="タイトルを初期値から変更する"]').click
+    find('[aria-label="各数値データのタイトル"]').find("#record_title_1").find('[aria-label="タイトルを初期値から変更する"]').click
     has_field?("record_title[title]")
     fill_in "record_title[title]", with: "ケナフ2"
     click_on "更新"
     has_no_field?("record_title[title]")
 
-    assert_text "ケナフ2"
+    within 'div[aria-label="各数値データのタイトル"] turbo-frame#record_title_1' do
+      assert_text "ケナフ2"
+    end
   end
 
   test "タイトルの編集" do
     sign_in_as(users(:alice))
     click_button "グラフ・表・メモの表示更新"
-    assert_text "alice's title 1"
+    within 'div[aria-label="各数値データのタイトル"] turbo-frame#record_title_0' do
+      assert_text "alice's title 1"
+    end
 
-    find('[aria-label="各数値データのタイトル"]').first('[aria-label="タイトルを編集する"]').click
+    find('[aria-label="各数値データのタイトル"]').find("#record_title_0").find('[aria-label="タイトルを編集する"]').click
     has_field?("record_title[title]")
     fill_in "record_title[title]", with: "ケナフ1"
     click_on "更新"
     has_no_field?("record_title[title]")
 
-    assert_no_text "alice's title 1"
-    assert_text "ケナフ1"
+    within 'div[aria-label="各数値データのタイトル"] turbo-frame#record_title_0' do
+      assert_text "ケナフ1"
+    end
   end
 
   test "タイトルのリセット" do
     sign_in_as(users(:alice))
     click_button "グラフ・表・メモの表示更新"
-    assert_text "alice's title 1"
+    within 'div[aria-label="各数値データのタイトル"] turbo-frame#record_title_0' do
+      assert_text "alice's title 1"
+    end
 
-    find('[aria-label="各数値データのタイトル"]').first('[aria-label="タイトルをリセットする"]').click
+    find('[aria-label="各数値データのタイトル"]').find("#record_title_0").find('[aria-label="タイトルをリセットする"]').click
     has_text?("本当にリセットしますか？")
     click_on "OK"
     has_no_text?("本当にリセットしますか？")
 
-    assert_no_text "alice's title 1"
+    within 'div[aria-label="各数値データのタイトル"] turbo-frame#record_title_0' do
+      assert_text "No.1"
+    end
   end
 end
