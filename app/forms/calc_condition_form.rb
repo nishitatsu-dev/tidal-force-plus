547891692988219a91b798452957e234ff6d08ad
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-02-10 16:48:48 +0900

    日付の入力規制をビューだけで行なっていた所を、FormObjectにバリデーションを追加した。併せて、入力可能な期間の設定を変更した

--- app/forms/calc_condition_form.rb
+++ app/forms/calc_condition_form.rb
@@ -7,4 +7,26 @@ class CalcConditionForm
   validates :first_date, presence: true
   validates :last_date, presence: true
   validates :timezone, presence: true
+  validate :date_limit
+
+  def date_limit
+    first = Date.parse(first_date) rescue nil
+    last = Date.parse(last_date) rescue nil
+    return if first.nil? || last.nil?
+
+    min = Date.new(2017, 1, 1)
+    max = Date.new(2060, 12, 31)
+
+    if last < first
+      errors.add(:base, "・期間の「終了日」は「開始日」以降の日付を選択してください。")
+    end
+
+    if first < min || last > max
+      errors.add(:base, "・期間は2017年1月1日から2060年12月31日までの範囲内で選択してください。")
+    end
+
+    if last > first + 10.years
+      errors.add(:base, "・期間は最大10年間です。")
+    end
+  end
 end
