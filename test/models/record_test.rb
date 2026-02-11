492e52c865b907e1173e03ec72b86e6ee9bb02e4
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-02-11 15:08:37 +0900

    モデルのスコープのテストを追加した

--- test/models/record_test.rb
+++ test/models/record_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class RecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "指定した日付の全レコードを取得できる" do
+    today = Date.current.strftime("%Y/%m/%d")
+    records_today = users(:carol).records.records_on(today)
+    records_all = users(:carol).records
+
+    assert_equal records_today.size, 3
+    assert_not_equal records_today.size, records_all.size
+  end
 end
