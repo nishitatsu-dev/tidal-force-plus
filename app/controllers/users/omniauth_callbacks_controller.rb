ea8d72be6f3f6f62544b90aa66f686a10ceff3c3
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-03-02 12:58:36 +0900

    継承元を間違えていたので修正した

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Users::OmniauthCallbacksController < ApplicationController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :authenticate_user!
   skip_before_action :verify_authenticity_token, only: :google_oauth2
 
