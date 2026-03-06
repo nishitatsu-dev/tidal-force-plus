a7c0f91417ea910df8306821b3e6867b89316eb0
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-03-06 16:16:31 +0900

    SnsCredentialのデータが保存されていないのを修正し、ログインで使われるようにした

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,3 +1,14 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user
+
+  def self.find_or_create_from_omniauth(auth)
+    sns_credential = SnsCredential.find_by(provider: auth.provider, uid: auth.uid)
+
+    unless sns_credential
+      user = User.create(email: auth.info.email, password: Devise.friendly_token[0, 20])
+      sns_credential = user.sns_credentials.create(provider: auth.provider, uid: auth.uid)
+    end
+
+    sns_credential
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,10 @@ class User < ApplicationRecord
   has_many :records, dependent: :destroy
   has_many :record_titles, dependent: :destroy
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data["email"]).first
+  def self.from_omniauth(auth)
+    sns_credential = SnsCredential.find_or_create_from_omniauth(auth)
+    sns_credential.user.update(email: auth.info.email) if sns_credential.user.email != auth.info.email
 
-    unless user
-      user = User.create(email: data["email"], password: Devise.friendly_token[0, 20])
-    end
-    user
+    sns_credential.user
   end
 end
