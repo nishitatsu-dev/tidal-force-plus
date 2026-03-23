c11dd47315fc2265067d2be9e2091869f48bbc25
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-03-23 20:29:50 +0900

    トランザクションを設定した。併せて早期リターンする形にした。

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -4,9 +4,11 @@ class SnsCredential < ApplicationRecord
   def self.find_or_create_from_omniauth(auth)
     sns_credential = SnsCredential.find_by(provider: auth.provider, uid: auth.uid)
 
-    unless sns_credential
-      user = User.create(email: auth.info.email, password: Devise.friendly_token[0, 20])
-      sns_credential = user.sns_credentials.create(provider: auth.provider, uid: auth.uid)
+    return sns_credential if sns_credential
+
+    ActiveRecord::Base.transaction do
+      user = User.create!(email: auth.info.email, password: Devise.friendly_token[0, 20])
+      sns_credential = user.sns_credentials.create!(provider: auth.provider, uid: auth.uid)
     end
 
     sns_credential
