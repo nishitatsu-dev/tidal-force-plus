c8cfc4f796e4270557f933914de2ce4e27026a14
nishitatsu-dev <nishitatsu.dev@gmail.com>
2026-03-23 20:23:07 +0900

    継承元を修正したので、不要なコールバックを削除した

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,4 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :authenticate_user!
   skip_before_action :verify_authenticity_token, only: :google_oauth2
 
   def google_oauth2
